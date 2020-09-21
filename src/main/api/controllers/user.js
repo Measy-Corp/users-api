e9646e1cddb71b928429262ee613deec5152fd98
Murilo Parra Pires <33543667+mupapi@users.noreply.github.com>
2020-09-20 22:45:51 -0300

    added the remaining routes (without session id for auth) (#3)

--- src/main/api/controllers/user.js
+++ src/main/api/controllers/user.js
@@ -1,14 +1,31 @@
-const { body, validationResult } = require('express-validator');
-const User = require('../../infrastructure/models/user')
+const { body, param, validationResult } = require('express-validator');
+const User = require('../../infrastructure/models/user');
 
 const validators = {
   "basic": [
-    body('id', 'id must be set').exists(),
-    body('firstName', 'firstName must be set').exists(),
+    body('id','id must be a valid UUID').isUUID(),
+    body('firstName', 'firstName must be set').optional().exists(),
     body('username', 'username must be set').exists(),
     body('email', 'email is invalid').isEmail(),
     body('password', 'password must have a min. length of 5').isLength({ min: 5 }),
     body('phone').optional().isInt()
+  ],
+  "auth": [
+    body('username', 'username must be set').exists(),
+    body('password', 'password must be set').exists(),
+  ],
+  "patch": [
+    body('id','id must be a valid UUID').isUUID(),
+    body('username', 'username is not writeable').isEmpty(),
+    body('email', 'email is invalid').optional().isEmail(),
+    body('password', 'password must have a min. length of 5').optional().isLength({ min: 5 }),
+    body('phone').optional().isInt()
+  ],
+  "id":[
+    param('id','id must be a valid UUID').isUUID()
+  ],
+  "username":[
+    param('username','username must be set').exists(),
   ]
 }
 
@@ -24,5 +41,54 @@ exports.createUser = (req, res, next) => {
   }
 
   User.create(req.body)
+      .then(user => res.json(user));
+}
+
+exports.authUser = (req, res, next) => {
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  User.findOne({where : {username : req.body.username}})
+      .then(user => {
+        console.log(user.password + ' == ' + req.body.password + '?');
+        res.json(user.password == req.body.password)
+      });
+}
+
+exports.updateUser = (req, res, next) => {
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  User.update(req.body, {where : {id : req.params.id}, returning: true, plain: true})
+      .then(result => {
+        res.json(result[1]);
+      });
+}
+
+exports.getUserByID = (req, res, next) => {
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  User.findByPk(req.params.id)
+      .then(user => res.json(user));
+}
+
+exports.getUserByUsername = (req, res, next) => {
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  User.findOne({where : {username : req.params.username}})
       .then(user => res.json(user));
 }
--- src/main/api/routes/user.js
+++ src/main/api/routes/user.js
@@ -6,4 +6,24 @@ router.post('/user',
     userController.createUser
 );
 
+router.post('/user/auth', 
+    userController.validate('auth'), 
+    userController.authUser
+);
+
+router.patch('/user/:id', 
+    userController.validate('patch'), 
+    userController.updateUser
+);
+
+router.get('/user/:id',
+    userController.validate('id'), 
+    userController.getUserByID
+);
+
+router.get('/user/username/:username',
+    userController.validate('username'), 
+    userController.getUserByUsername
+);
+
 module.exports = router;
