bdb9f089d08a3391b615e214ce927c73d66cccaf
Murilo Kakazu <yuri.kakazu@gmail.com>
2020-09-24 22:58:29 -0300

    fix: set optional data when patching product (#13)

--- src/main/api/product/controller.js
+++ src/main/api/product/controller.js
@@ -18,11 +18,11 @@ const validators = {
     body('unitPrice', 'unitPrice is invalid').isFloat({ min: 0.0 })
   ],
   "patch": [
-    body('storeId','storeId must be a valid UUID').isUUID(),
-    body('name', 'name must have a min. length of 3').isLength({ min: 3 }),
-    body('description', 'name must have a min. length of 10').isLength({ min: 10 }),
-    body('category', 'category is empty').exists(),
-    body('unitPrice', 'unitPrice is invalid').isFloat({ min: 0.0 })
+    body('storeId','storeId must be a valid UUID').optional().isUUID(),
+    body('name', 'name must have a min. length of 3').optional().isLength({ min: 3 }),
+    body('description', 'name must have a min. length of 10').optional().isLength({ min: 10 }),
+    body('category', 'category is empty').optional(),
+    body('unitPrice', 'unitPrice is invalid').optional().isFloat({ min: 0.0 })
   ],
   "id":[
     param('id','id must be a valid UUID').isUUID()
