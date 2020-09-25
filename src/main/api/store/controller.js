5c0a5c7d5296462c727ef86a3e961f93e860fedf
Murilo Kakazu <yuri.kakazu@gmail.com>
2020-09-24 23:21:12 -0300

    fix: add store valdiations (#14)

--- src/main/api/store/controller.js
+++ src/main/api/store/controller.js
@@ -2,6 +2,48 @@ const { body, param, check, validationResult } = require('express-validator');
 const Store = require('../../infrastructure/models/store');
 const { Op } = require('sequelize');
 
+const validators = {
+  "post": [
+    body('id','id must be a valid UUID').isUUID(),
+    check('id').custom((value, { req }) => {
+      return Store.findByPk(value).then(store => {
+        if (store) {
+            return Promise.reject('id already exists');
+        }
+      })
+    }),
+    body('ownerId', 'ownerId must be a valid UUID').isUUID(),
+    body('document', 'document must be set').exists(),
+    body('name', 'name must have a min. length of 3').isLength({ min: 3 }),
+    body('phone', 'name must be set').exists(),
+    body('address', 'category must be set').exists(),
+    body('email', 'email is invalid').isEmail(),
+    body('type', 'type must be set').exists(),
+  ],
+  "patch": [
+    body('ownerId', 'ownerId must be a valid UUID').optional().isUUID(),
+    body('document', 'document must be set').optional().exists(),
+    body('name', 'name must have a min. length of 3').optional().isLength({ min: 3 }),
+    body('phone', 'name must be set').optional().exists(),
+    body('address', 'category must be set').optional().exists(),
+    body('email', 'email is invalid').optional().isEmail(),
+    body('type', 'type must be set').optional().exists(),
+  ],
+  "getById": [
+    param('id', 'id must be a valid UUID').isUUID()
+  ],
+  "getByName": [
+    param('name', 'name must have a length of at least 4').isLength({ min: 4 })
+  ],
+  "getByType": [
+    param('type', 'type must be set').exists()
+  ]
+}
+
+exports.validate = (validator) => {
+  return validators[validator];
+}
+
 exports.createStore = (req, res, next) => {
 
   const errors = validationResult(req);
--- src/main/api/store/route.js
+++ src/main/api/store/route.js
@@ -4,26 +4,31 @@ const auth = require('../auth/controller');
 
 router.post('/store',
     auth.verify,
+    storeController.validate('post'),
     storeController.createStore
 );
 
-router.patch('/store:id',
+router.patch('/store/:id',
     auth.verify,
+    storeController.validate('patch'),
     storeController.updateStore
 );
 
 router.get('/store/:id',
     auth.verify,
+    storeController.validate('getById'),
     storeController.getStoreByID
 );
 
 router.get('/store/name/:name',
     auth.verify,
+    storeController.validate('getByName'),
     storeController.getStoresByName
 );
 
 router.get('/store/type/:type',
     auth.verify,
+    storeController.validate('getByType'),
     storeController.getStoresByType
 );
 
